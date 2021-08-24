9977dcef10fe5348c6129f1c87d7f4c2989bce41
IsabelleVasconcelos <88832627+IsabelleVasconcelos@users.noreply.github.com>
2021-08-24 13:20:28 -0300

    Add files via upload

---
+++ aula 5 tipos compostos.py
@@ -0,0 +1,17 @@
+# lista = []
+# print (lista)
+# lista = list ()__
+# print (lista)
+
+#linha 1 e linha 3 são exemplos de como montar uma lista vazia sem a hastag
+
+#exemploDeLista = ['isabelle', 123, 1.5]
+#print (exemploDeLista)
+#exemplo de lista dentro de lista anotado no slide, bem como acessar cada item da lista
+
+listaNomes = ['','']
+listaNomes[0] = input('digite uma marca de maquiagem\n')
+listaNomes[1] = input ('digite outra marca de maquiagem\n')
+print (listaNomes)
+listaNomes = ['isabelle, camilla, yuri', listaNomes]
+
