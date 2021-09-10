723366fdc4f6c46a63cd8960dbfa21102debb82c
IsabelleVasconcelos <88832627+IsabelleVasconcelos@users.noreply.github.com>
2021-09-10 07:09:10 -0300

    Create ListaAula6.py

---
+++ ListaAula6.py
@@ -0,0 +1,77 @@
+INCOMPLETO
+As entradas (seja informações de máquina, posicionamentos a serem medidos etc.) serão traduzidas em inputs no programa.
+O detalhamento das etapas é informado na tela para o usuário a cada passo do procedimento. FAZER PRINT DE CADA PROCEDIMENTO
+O programa deve conter estruturas de tomada de decisão (if-elif-else)./ Sempre colocar o tab pq se não da erro sintatico -> AULA 4
+O programa deve conter estruturas de repetição (while-for).-> AULA 6
+O programa deve conter pelo menos 1 variável de cada tipo (lista{coleção ordenada USA COLCHETES}, tupla{sequencia de valores, USA PARENTESES} e dicionário{mapeamento entre uma 
+chaveKEY e algum conteudoVALUE})-> AULA 5
+
+
+  print("Olá! Aqui vão as orientações para a realização da cirurgia!")
+  
+  print("Vamos iniciar o procedimento da anestesia...")
+  print("Selecione o tipo de anestesia que você se planejou para usar:)
+    botao1Apertado = int(input("Você escolheu Ketamina e xilazina para serem utilizados utilizados em conjunto")
+    botao2Apertado = int(input("Você escolheu halotano (gasoso).")
+  print("Lembre-se de verificar a dosagem correta de acordo com o peso dos animais!")
+  
+  print("Fim do processo de anestesia") 
+
+2-Depois do anestésico ter feito efeito, deve-se posicionar o animal no estereotáxico. As barras que suportam o peso do animal devem ser posicionadas no ouvido externo do animal.
+Normalmente o animal dá uma pequena piscada, devido ao estímulo da musculatura responsável por este movimento. Em seguida verificar a angulação da cabeça do animal, a qual deve estar sem diferenças de angulação entre o bregma e o lambda, para ter uma superfície de cirurgia plana.
+
+3- Limpeza do campo de trabalho: Este procedimento requer o cumprimento de algumas etapas: Retirada da pelagem que recobre a parte superior da calota craniana, Retirada dos tecidos
+moles (epiderme, derme e tecido conjuntivo) até alcançar a parte óssea da caixa craniana. Por último e não menos importante deve-se limpar a calota craniana de qualquer resto de 
+“pele” que esteja sobrando utilizando H2O2 10 volumes.
+
+4-Com o animal em posição e com o campo cirúrgico devidamente limpo, utiliza-se uma pequena camada de poliacrilato em todo o perímetro externo para evitar sangramentos.
+
+5-Após este procedimento deve-se escolher um ponto para a fixação de parafusos, de preferência na parte posterior da calota craniana, pois a camada óssea é mais espessa e suporta 
+uma maior profundidade do parafuso. 
+Obs: Cuidar para não aprofundar muito o parafuso. Com parafusos maiores deve-se dar até 3 voltas no parafuso.
+Posicionar a agulha (devidamente preparada para o tamanho da cânula e que servirá de suporte para a fixação das cânulas) sobre o bregma (ver figura acima). Fazer os cálculos de 
+posicionamento AnteroPosterior (AP), LateroLateral (LL) e DorsoVentral (DV). Os valores utilizados para os cálculos são os valores encontrados nas réguas a partir do posicionamento
+da agulha.
+E. G.: Hipocampo (CA1): Valores hipotéticos para fins de cálculo.
+AP: 6,42 cm 
+LL: 3,23 cm
+DV: 4,2 cm
+Isso significa que estes foram os valores encontrados em cada régua do estereotáxico e se subtrai ou soma a estes, os valores de cada estrutura ou núcleo. 
+CA1: AP - 0,42 cm LL +/- 0,30 cm DV - 0,20 cm.
+AP: 6,42 cm – 0,42 = 6,00
+LL: 3,33 cm + 0,30 = 3,63 / - 0,30 = 3,03  (BILATERAL)
+DV: 4,20 cm – 0,20 = 4,00
+
+6-Após estes cálculos feitos é hora de localizar os pontos de inserção das cânulas-guia. Assim que estes pontos forem localizados é necessário fazer furos para a introdução das 
+cânulas-guia. A localização destes pontos deve ser definida pelos valores encontrados nos cálculos AnteroPosterior (6,00) e LateroLateral (3,63 e 3,03). Deve-se escolher qual dos
+hemisférios vai ser colocada a primeira cânula, daí os dois valores para as medidas LL.
+
+7-Depois de posicionar a agulha, fazer um furo com a broca até alcançar as meninges. A não perfuração das meninges é o procedimento ideal, e para conseguir isso apoie a mão que 
+segura a broca contra o assoalho ou ao estereotáxico e perfure o crânio a +- 450 de angulação.
+
+8-Após ter atingido este objetivo, introduza a cânula-guia previamente confeccionada até o valor DorsoVentral (4,00) que foi calculado anteriormente.
+
+9-Logo após drenar qualquer sangue ou líquido cefalorraquidiano que esteja saindo pelo orifício criado no crânio. Para isso utilize pequenos rolos de papel absorvente.
+
+10-Faça uma mistura do acrílico polimerizante com o solvente até ficar com textura espessa porém maleável (o ideal é que a mistura seja capaz de cobrir a parte desejada sem 
+escorrer por todo o crânio). Com essa mistura faça um capacete abrangendo o crânio, a cânula-guia e o parafuso. Deixe secar até ficar suficientemente rígido. O tempo de secagem 
+varia de acordo com a temperatura e umidade da sala. 
+
+11- O próximo passo é a fixação da outra cânula-guia. Deve-se levantar levemente o braço do estereotáxico cuidando para que a cânula-guia previamente fixada não se movimente. 
+Logo após, posicionar a agulha sobre o outro ponto de inserção da cânula-guia. Introduzir a cânula-guia até o valor DV (4,00) calculado previamente.
+
+12- Seguir novamente a descrição do item 9 e após fixar a cânula conforme item 10. De preferência espalhar o cimento sobre a maior área do crânio, sempre deixando um espaço entre
+o capacete e o início da área tecidual. Este cuidado previne de um futuro descolamento do capacete devido a entrada de sangue u outro líquido entre o capacete e o crânio.
+
+13-Levantar bem devagar seguindo as instruções do item contida no item 11. Acomodar o animal em uma caixa aquecida por uma lâmpada e sem outros animais acordados. Assim que o 
+animal despertar colocá-lo de volta a sua caixa-moradia.
+
+
+
+
+
+
+
+
+
+
