644e2240dbdb1f43914ad54f4f10df70f9f16741
IsabelleVasconcelos <88832627+IsabelleVasconcelos@users.noreply.github.com>
2021-10-01 16:47:41 -0300

    Add files via upload
    Atividades

--- ATIVIDADES/AULA 3/aula3.py
+++ ATIVIDADES/AULA 3/aula3.py
@@ -35,13 +35,3 @@ print (7//2, 2**10, 7%2) #parte inteira, potenciação, resto da divisão
 
 soma = 0
 soma += 3; print(soma); soma= soma+3; print(soma)
-
-
-
-num = input ("digite um número:")
-print(num)
-
-login = input ("login:")
-senha = input ("senha:")
-
-print ("o usuário informado foi: %s, e a senha digitada foi: %s" %(login, senha))
